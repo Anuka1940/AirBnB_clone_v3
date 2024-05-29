346230bfc5f48bd5238f38b25623c67206709e35
Abdul Lateef Anuka <anukalateef@gmail.com>
2024-05-29 23:09:23 +0100

    Update file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""Create the Flask application and register blueprints"""
-import os
-from flask import Flask, jsonify
+"""Create a web application using Flask
+"""
 from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
 from models import storage
-
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.debug = True
 
 
 @app.teardown_appcontext
 def close(error):
-    """Close the storage session."""
+    """Close storage after every session"""
     storage.close()
 
 
 @app.errorhandler(404)
-def not_found(e):
+def not_found(error):
     """Return json object for 404 not found errorr"""
-    return jsonify({
-                    "error": "Not found"
-                    }), 404
+    return make_response(jsonify({
+        "error": "Not found"
+    }), 404)
+
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = os.environ.get("HBNB_API_PORT", "5000")
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = os.environ.get('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
